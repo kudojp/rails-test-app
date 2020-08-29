224989724af1516aac32aaed4a31690eb403ec6e
kudojp <heyjudejudejude1968@gmail.com>
2020-08-29 16:57:31 +0900

    Use byebug to debug

--- app/controllers/my_test_controller.rb
+++ app/controllers/my_test_controller.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class MyTestController < ApplicationController
+  before_action do
+    require 'pry'; binding.pry
+  end
+
   def env_var
     render json: { should_show_in_app_purchase: ENV['ENABLE_APPLE_IAP'].to_d }
   end
